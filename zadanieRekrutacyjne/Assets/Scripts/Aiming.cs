0f0850a8780169131b28b957df9e50cb02cfba75
SpaceAndFrogs <73168676+SpaceAndFrogs@users.noreply.github.com>
2020-11-24 19:18:39 +0100

    AimingAndRotation
    Simple line for aiming is done.
    Rotating with swaping is done.

---
+++ zadanieRekrutacyjne/Assets/Scripts/Aiming.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour
+{
+    public LineRenderer line;
+    public GameObject shootPoint1;
+    public GameObject shootPoint2;
+    void Start()
+    {
+        line.SetPosition(0, shootPoint1.transform.position);
+
+        line.SetPosition(1, shootPoint2.transform.position);
+    }
+    private void FixedUpdate()
+    {
+        line.SetPosition(0, shootPoint1.transform.position);
+        line.SetPosition(1, shootPoint2.transform.position);
+    }
+}
---
+++ zadanieRekrutacyjne/Assets/Scripts/Rotating.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+    private Touch touch;
+
+    private Vector2 touchPosition;
+
+    private Quaternion rotationY;
+
+    public float rotationSpeed;
+ 
+    void FixedUpdate()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                rotationY = Quaternion.Euler(0f, touch.deltaPosition.x * rotationSpeed, 0f);
+
+                transform.rotation *= rotationY;
+            }
+        }
+    }
+}
